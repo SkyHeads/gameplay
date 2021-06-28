dfb04a51749a1d76683861c37ae2ac30cbc19ca3
SkyHeads <skyheads@live.com>
2021-06-28 02:14:03 -0300

    install expo-font inter/rajdhani and expo-loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import { SignIn } from './src/screens/SignIn';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+
   return (
     <>
       <StatusBar style='light'/>
