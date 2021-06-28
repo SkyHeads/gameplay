127bb4486f3c13b7f467ec5fb37dcf25a09f6ca6
SkyHeads <skyheads@live.com>
2021-06-28 01:54:28 -0300

    init app/splash/assets/theme/screens singin

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { 
+  View,
+  Text, 
+  Image 
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { styles } from './styles';
+
+export const SignIn: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image}
+        resizeMode='stretch'
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          Jogatinas
+        </Text>
+
+        <Text style={styles.subTitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord" 
+          activeOpacity={0.7}
+        />
+      </View>
+    </View>
+  );
+}
