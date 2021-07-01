cd189726fee410d54768a3182ecba863f9e31516
SkyHeads <skyheads@live.com>
2021-07-01 00:55:12 -0300

    import categoryselect/category state and select

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { 
   View,
   Text
 } from 'react-native';
 import { Background } from '../../components/Background';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
 
 export const Home: React.FC = () => {
+  const [category, setCategory] = useState('');
+
+  const handleCategorySelect = useCallback((categoryId: string) => {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }, []);
+
   return (
     <Background>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+      
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect} 
+        />
       </View>
     </Background>
   );
