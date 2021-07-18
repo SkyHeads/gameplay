54abf4ba6b2c1b4d0192a503c45a5992ac425886
SkyHeads <skyheads@live.com>
2021-07-18 17:28:57 -0300

    adjust category/styles/hascheckbox props

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -12,12 +12,14 @@ type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
   checked?: boolean;
+  hasCheckBox?: boolean;
 }
 
 export const Category: React.FC<Props> = ({ 
   title, 
   icon: Icon, 
-  checked = false, 
+  checked = false,
+  hasCheckBox = false, 
   ...rest 
 }) => {
   const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
@@ -28,15 +30,22 @@ export const Category: React.FC<Props> = ({
         style={styles.container}
         colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? styles.checked : styles.check} />
-         
+        <LinearGradient 
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[ checked ? secondary85 : secondary50, secondary40]}
+        > 
+          {
+            hasCheckBox && 
+            <View 
+              style={checked ? styles.checked : styles.check} 
+            />
+          }
           <Icon width={48} height={48} />
 
           <Text style={styles.title}>
             { title }
           </Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
