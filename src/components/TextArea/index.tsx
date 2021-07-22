f81aecf587fda5fec5d4b1d585bdaa213ecb211a
SkyHeads <skyheads@live.com>
2021-07-22 02:44:42 -0300

    small input/text area/button component

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -3,12 +3,11 @@ import { TextInput, TextInputProps } from 'react-native';
 
 import { styles } from './styles';
 
-export const SmallInput: React.FC<TextInputProps> = ({ ...rest }) => {
+export const TextArea: React.FC<TextInputProps> = ({ ...rest }) => {
   return (
     <TextInput
       style={styles.container}
       {...rest}
-      keyboardType="numeric"
     />
   );
 }
