377d3b96f81fdddb61b6ef289feca0d38e6ccc83
Bruno <skyheads@live.com>
2021-08-12 12:24:19 -0300

    adjust profile component user avatar+firstname

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useAuth } from '../../hooks/auth';
 
 import { Avatar } from '../Avatar';
 
 import { styles } from './styles';
 
-export const Profile: React.FC = () => (
-  <View style={styles.container}>
-    <RectButton onPress={() => console.log('Profile Button')}>
-      <Avatar urlImage="https://github.com/skyheads.png" />
-    </RectButton>
+export const Profile: React.FC = () => {
+  const { user } = useAuth();
 
-    <View>
-      <View style={styles.user}>
-        <Text style={styles.greeting}>Olá,</Text>
+  return (
+    <View style={styles.container}>
+      <RectButton onPress={() => console.log('Profile Button')}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
 
-        <Text style={styles.username}>Bruno</Text>
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá,</Text>
+
+          <Text style={styles.username}>{user.firstName}</Text>
+        </View>
+
+        <Text style={styles.message}>Hoje é dia de vitória</Text>
       </View>
-
-      <Text style={styles.message}>Hoje é dia de vitória</Text>
     </View>
-  </View>
-);
+  );
+};
