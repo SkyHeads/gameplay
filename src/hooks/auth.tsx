3364bd177fb59eb13a5f2de135b029c9a1692038
Bruno <skyheads@live.com>
2021-08-18 19:46:03 -0300

    asyncstorage setitem and getitem

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,16 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react';
 import * as AuthSession from 'expo-auth-session';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { api } from '../services/api';
+import { COLLECTION_USERS } from '../config/storage';
 
 const { CDN_IMAGE } = process.env;
 const { CLIENT_ID } = process.env;
@@ -57,11 +67,15 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-        setUser({
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token,
-        });
+        };
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+
+        setUser(userData);
       }
     } catch {
       throw new Error('Error in Authentication');
@@ -69,6 +83,21 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       setLoading(false);
     }
   }
+
+  async function loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+    if (storage) {
+      const userLogged = JSON.parse(storage) as User;
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, signIn, loading }}>
